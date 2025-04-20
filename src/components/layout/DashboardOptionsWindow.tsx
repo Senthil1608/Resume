b5297e25972ec3622f1a5e495d13fa01bc24014e
Senthil1608 <senthind26@gmail.com>
2025-04-20 19:24:39 +0530

    Dashbard with All Projects view created

--- src/components/layout/DashboardOptionsWindow.tsx
+++ src/components/layout/DashboardOptionsWindow.tsx
@@ -4,13 +4,13 @@ function DashboardOptionsWindow()
 {
     return(
         <>
-            <div className="w-64 flex-grow bg-gradient-to-b from-[#1E293B] to-[#5270A1]">
+            <div className="w-86 flex-grow h-screen bg-gradient-to-b from-[#1E293B] to-[#5270A1]">
                 <div className="flex flex-col justify-center space-y-8 my-8 items-center">
                  <button className="bg-white py-2 px-6 w-fit text-2xl font-medium"> New Project</button>
                  <button className="bg-[#3B5C92] py-2 px-10 w-56 text-xl text-white "> All Projects</button>
                  <button className="bg-white py-1 px-10 w-56 text-xl "> My Projects</button>
                  <button className="bg-white py-1 px-10 w-56 text-xl "> Shared Projects</button>
-                 <button className="bg-white py-1 px-10 w-56 text-xl "> Deleted Projects</button>
+                 <button className="bg-white py-1 px-10 w-56 text-xl text-red-600 "> Deleted Projects</button>
                 </div>
             </div>
         </>
