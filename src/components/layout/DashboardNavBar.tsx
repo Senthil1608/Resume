6eea643d9d546c5a17d5bd5d24e589080eae225d
Senthil1608 <senthind26@gmail.com>
2025-04-27 16:12:21 +0530

    temporary dashboard commit

--- src/components/layout/DashboardNavBar.tsx
+++ src/components/layout/DashboardNavBar.tsx
@@ -6,10 +6,10 @@ function DashboardNavBar()
     return(
         <>
          <div className="flex flex-row items-center justify-between px-4  bg-white py-2 border-b-black border-b-1">
-         <a href="/"><img src={Logo} className="h-16 w-auto"/></a>
-         <div className="flex flex-row items-center px-4">
-            <h2 className="text-4xl">P S Senthil Srinivas</h2>
-            <div className="w-16 h-16 bg-white border-2 border-black rounded-full ml-8 overflow-hidden flex items-center justify-center">
+         <a href="/"><img src={Logo} className="h-14 w-auto"/></a>
+         <div className="flex flex-row items-center px-8">
+            <h2 className="text-2xl">P S Senthil Srinivas</h2>
+            <div className="w-12 h-12 bg-white border-2 border-black rounded-full ml-4 overflow-hidden flex items-center justify-center">
                 <img src={senthil} className="w-full h-full object-cover" />
             </div>
         </div>
--- src/components/layout/ProjectView.tsx
+++ src/components/layout/ProjectView.tsx
@@ -28,8 +28,8 @@ function ProjectView()
     const [tempText, setTempText] = useState(displayText);
     return(
         <>
-            <div className="bg-white border-black border-1 p-4 mx-8 flex flex-row justify-between items-center">
-                <div className="flex flex-row items-center px-4 space-x-4">
+            <div className="bg-white border-black border-1 p-4 mx-8 flex flex-row justify-between items-center w-3xl">
+                <div className="flex flex-row items-center px-4 space-x-4 w-fit">
                 {isEditing ? (
                     <input
                         type="text"
@@ -42,16 +42,16 @@ function ProjectView()
                     />
                  ) : (
                 <>
-                <h3 className="text-2xl">{displayText}</h3>
-                <MdModeEdit className="text-3xl text-[#000000] cursor-pointer" onClick={handleEditClick} />
+                <h3 className="text-l">{displayText}</h3>
+                <MdModeEdit className="text-2xl text-[#000000] cursor-pointer" onClick={handleEditClick} />
             </>
         )}
                 </div>
                  <div className="flex flex-row items-center px-4 space-x-8">
-                    <MdOutlineFileDownload className="text-4xl text-green-600" />
-                    <FaShare className="text-3xl text-gray-800"  />
-                    <button className="bg-[#fd1d1d] py-2 px-10 text-xl text-white ">Delete </button>
-                    <a className="bg-[#3B5C92] py-2 px-10 text-xl text-white " href="/Editor">Edit </a>
+                    <MdOutlineFileDownload className="text-xl text-green-600" />
+                    <FaShare className="text-l text-gray-800"  />
+                    <button className="bg-[#fd1d1d] py-2 px-4 text-sm text-white ">Delete </button>
+                    <a className="bg-[#3B5C92] py-2 px-4 text-sm text-white " href="/Editor">Edit </a>
                 </div>
             </div>
         </>
